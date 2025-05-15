951d14d1e8f34f790adfd50bdc2ec8c3f4b28ae5
Vik3sh <145530156+Vik3sh@users.noreply.github.com>
2025-05-15 14:38:25 +0530

    Create Enemy.cs
    in future if u want to add more enemies that i will do so this  code will help in the basic movement and the death animations

---
+++ Enemy.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] protected GameObject damageTrigger;
+    [Space]
+    protected Animator anim;
+    protected Rigidbody2D rb;   
+    [SerializeField] protected float moveSpeed =2f;
+    [SerializeField] protected float idleDuration =1.5f;
+    [SerializeField]protected float idleTimer;
+    [Header("Death Details")]
+    [SerializeField] private float deathImpactSpeed=5;
+    [SerializeField] private float deathRotaitionSpeed=150;
+    private int deathRotationDirection = 1;
+    protected bool isDead;
+    [Header("Basic collision")]
+    [SerializeField] protected float groundCheckDistance = 1.1f;
+    [SerializeField] protected float wallCheckDistance = .7f;
+    [SerializeField] protected LayerMask whatIsGround;
+    [SerializeField] protected Transform groundCheck;
+    protected bool isGrounded;
+    protected bool isGroundInFrontDetected;
+    protected bool isWallDetected;
+
+    protected int facinDir = -1;
+    protected bool facingRight = false;  
+
+
+    protected virtual void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    protected virtual void Update()
+    {
+        idleTimer -= Time.deltaTime;
+
+        if (isDead)
+        {
+            HandleDeath();
+        }
+    }
+
+    public virtual void Die()
+    {
+        damageTrigger.SetActive(false); 
+        anim.SetTrigger("Hit");
+        rb.velocity=new Vector2(rb.velocity.x, deathImpactSpeed); 
+        isDead = true;
+        if(Random.Range(0, 100)<50)
+        {
+            deathRotationDirection = deathRotationDirection * -1;
+        }
+    }
+    private void HandleDeath()
+    {
+        transform.Rotate(0, 0, (deathRotationDirection * deathRotaitionSpeed) * Time.deltaTime);
+    }
+    
+
+    
+    protected virtual void HandleFlip(float xValue)
+    {
+
+        if (xValue < 0 && facingRight || xValue > 0 && !facingRight)
+        {
+            flip();
+            idleTimer = idleDuration;
+        }
+
+    }
+    protected virtual void HandleCollision()
+    {
+        isGrounded= Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isGroundInFrontDetected = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+
+        isWallDetected = Physics2D.Raycast(transform.position, Vector2.right * facinDir, wallCheckDistance, whatIsGround);
+
+    }
+
+
+    protected virtual void flip()
+    {
+        facinDir = facinDir * -1;
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(groundCheck.position, new Vector2(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + (wallCheckDistance * facinDir), transform.position.y));
+    }
+}
